b4f490139395f597c5b2db8676bb25e3def6bef0
baxtiyar0393 <74107721+baxtiyar0393@users.noreply.github.com>
2020-12-11 18:37:48 +0400

    commit

--- src/main/java/az/mycompany/javalessons/lesson5/Armstrong.java
+++ src/main/java/az/mycompany/javalessons/lesson5/Armstrong.java
@@ -2,18 +2,18 @@ package az.mycompany.javalessons.lesson5;
 
 public class Armstrong {
     public static void main(String[] args) {
-        int s=0;
+
         for(int i=1;i<=500;i++){
-            int b=i;
-          while(b>0) {
-              int a = b % 10;
-               b = i / 10;
-              s=s+a*a*a;          }
-          if(s==i){
+            int a=i/100;
+            int b=(i%100)/10;
+            int c=((i%100)%10);
+              a=a*a*a+b*b*b+c*c*c;
+          if(i==a) {
               System.out.println(i);
+          }
 
           }
 
         }
     }
-}
+
