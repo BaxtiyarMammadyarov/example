3a5de01c56e74a0fce664da5e04757e298dfa5b5
baxtiyar0393 <74107721+baxtiyar0393@users.noreply.github.com>
2020-12-11 23:39:46 +0400

    commit

--- src/main/java/az/mycompany/javalessons/lesson5/WhileStm1.java
+++ src/main/java/az/mycompany/javalessons/lesson5/WhileStm1.java
@@ -6,22 +6,44 @@ public class WhileStm1 {
     public static void main(String[] args) {
         Scanner scn =new Scanner(System.in);
         int i=0,j=0,k=0;
+        String exit;
 
         while(true){
             System.out.print("ededi daxil edin :");
             int num=scn.nextInt();
-            System.out.print("davam etmek ucun : (y/n)");
-            String exit=scn.next();
-            if(num>0) i=i+1;
-            else if(num<0) j=j+1;
-            else if(num==0) k=k+1;
-             if (exit.equals("y")) {
+
+            if(num>0) {i=i+1;
+                System.out.print("Davam etme isteyirsiz : (y/n): ");
+                exit=scn.next();
+                if (exit.equals("y")) {
+                    continue;
+                }
+                else if (exit.equals("n")) {
+                    System.out.println("musbet : "+i+"\n"+"menfi : "+j+"\n"+"sifir: "+k);
+                    break;}
 
             }
-            else if (exit.equals("n")) {
-                System.out.println("musbet : "+i+"\n"+"menfi : "+j+"\n"+"sifir: "+k);
-                break;}
-            else System.out.println("secim duzgun deyil");
+            else if(num<0){ j=j+1;
+                System.out.print("Davam etme isteyirsiz : (y/n): ");
+                exit=scn.next();
+                if (exit.equals("y")) {
+                    continue;
+                }
+                else if (exit.equals("n")) {
+                    System.out.println("musbet : "+i+"\n"+"menfi : "+j+"\n"+"sifir: "+k);
+                    break;}
+            }
+            else if(num==0) {k=k+1;
+                System.out.print("Davam etme isteyirsiz : (y/n): ");
+                exit=scn.next();
+                if (exit.equals("y")) {
+                    continue;
+                }
+                else if (exit.equals("n")) {
+                    System.out.println("musbet : "+i+"\n"+"menfi : "+j+"\n"+"sifir: "+k);
+                    break;}
+            }
+
         }
 
 
