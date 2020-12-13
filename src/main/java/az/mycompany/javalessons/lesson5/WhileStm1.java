283dc439e07b2e2ac62692edec6b2cb2dd6cc353
baxtiyar0393 <74107721+baxtiyar0393@users.noreply.github.com>
2020-12-13 19:47:43 +0400

    commit

--- src/main/java/az/mycompany/javalessons/lesson5/WhileStm1.java
+++ src/main/java/az/mycompany/javalessons/lesson5/WhileStm1.java
@@ -8,47 +8,23 @@ public class WhileStm1 {
         int i=0,j=0,k=0;
         String exit;
 
-        while(true){
+        do {
             System.out.print("ededi daxil edin :");
             int num=scn.nextInt();
+            System.out.print("Programdan ayrilmaq iseyirsiz mi? : (y/n)");
+            exit=scn.next();
+            if(num>0) i=i+1;
+            else if(num<0) j=j+1;
+            else if(num==0)  k=k+1;
+            else System.out.println("Secim duzgun deyil!!!");
 
-            if(num>0) {i=i+1;
-                System.out.print("Davam etme isteyirsiz : (y/n): ");
-                exit=scn.next();
-                if (exit.equals("y")) {
-                    continue;
-                }
-                else if (exit.equals("n")) {
-                    System.out.println("musbet : "+i+"\n"+"menfi : "+j+"\n"+"sifir: "+k);
-                    break;}
-
-            }
-            else if(num<0){ j=j+1;
-                System.out.print("Davam etme isteyirsiz : (y/n): ");
-                exit=scn.next();
-                if (exit.equals("y")) {
-                    continue;
-                }
-                else if (exit.equals("n")) {
-                    System.out.println("musbet : "+i+"\n"+"menfi : "+j+"\n"+"sifir: "+k);
-                    break;}
-            }
-            else if(num==0) {k=k+1;
-                System.out.print("Davam etme isteyirsiz : (y/n): ");
-                exit=scn.next();
-                if (exit.equals("y")) {
-                    continue;
-                }
-                else if (exit.equals("n")) {
-                    System.out.println("musbet : "+i+"\n"+"menfi : "+j+"\n"+"sifir: "+k);
-                    break;}
-            }
-
-        }
-
-
-
-
-        }
+        } while (!(exit.equals("y")));
+        System.out.println("Positive : "+i+"\n"+"Negative : "+j+"\n" +"zero : "+k);
     }
+}
+
+
+
+
+
 
