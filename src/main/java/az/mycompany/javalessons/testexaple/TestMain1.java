d6bba97a0fabd856c42296701056c592386eb822
baxtiyar0393 <74107721+baxtiyar0393@users.noreply.github.com>
2020-12-17 15:10:33 +0400

    commit

--- src/main/java/az/mycompany/javalessons/testexaple/TestMain1.java
+++ src/main/java/az/mycompany/javalessons/testexaple/TestMain1.java
@@ -2,14 +2,15 @@ package az.mycompany.javalessons.testexaple;
 
 public class TestMain1 {
     public static void main(String[] args) {
-        Integer num;
-        for(num=1000;num<10000;num++){
-            int digit1=Integer.parseInt(num.toString().substring(0,1));
-            int digit2=Integer.parseInt(num.toString().substring(1,2));
-            int digit3=Integer.parseInt(num.toString().substring(2,3));
-            int digit4=Integer.parseInt(num.toString().substring(3,4));
+
+        for(int i=1000;i<10000;i++){
+            int digit1=Integer.parseInt(Integer.toString(i).substring(0,1));
+           int digit2=Integer.parseInt(Integer.toString(i).substring(1,2));
+            int digit3=Integer.parseInt(Integer.toString(i).substring(2,3));
+            int digit4=Integer.parseInt(Integer.toString(i).substring(3,4));
+
             if(digit1+digit2==digit3+digit4){
-                System.out.println(num);
+                System.out.println(i);
 
             }
 
