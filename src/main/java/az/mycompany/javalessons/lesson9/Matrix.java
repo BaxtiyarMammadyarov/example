ac79ed12c4a2f8fc58346ff7f226e65fe851aa62
baxtiyar0393 <74107721+baxtiyar0393@users.noreply.github.com>
2021-01-12 18:14:56 +0400

    commit

--- src/main/java/az/mycompany/javalessons/lesson9/Matrix.java
+++ src/main/java/az/mycompany/javalessons/lesson9/Matrix.java
@@ -4,19 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Matrix {
-  private List<List>matrix=new ArrayList<>();
+  private ArrayList<ArrayList>matrix=new ArrayList<>();
 
    public  void setMatrix(int number){
         for (int i=0;i<number;i++ )
-              matrix.add(0,inputArray(number));
-
+              matrix.add(0, inputArray(number));
         }
 
-        List<Integer>inputArray(int num) {
-           List <Integer> arr= new ArrayList<>();
+        ArrayList<Integer>inputArray(int num) {
+           ArrayList <Integer> arr= new ArrayList<>();
            for(int j=0;j<num;j++)
                    arr.add(j, (int) (Math.random() * 100));
-
            return arr;
        }
        public List<Integer> elementsLineMatrix(int indexline){
@@ -25,9 +23,6 @@ public class Matrix {
        public Integer elementMatrix(int indexline,int indexcolumn){
          return elementsLineMatrix(indexline).get(indexcolumn);
        }
-
-
-
    public void showMatrix(){
        for(List i:matrix)
            System.out.println(i.toString().replace(",","").replace("[","").replace("]",""));
@@ -35,12 +30,12 @@ public class Matrix {
 
     public static void main(String[] args) {
         Matrix matrix=new Matrix();
-        matrix.setMatrix(5);
+        matrix.setMatrix(4);
         matrix.showMatrix();
         System.out.println("-----------------------------------");
         System.out.println( matrix.elementsLineMatrix(3));
         System.out.println("------------------------------------");
-        System.out.println(matrix.elementMatrix(3,4));
+        System.out.println(matrix.elementMatrix(3,3));
 
     }
 }
